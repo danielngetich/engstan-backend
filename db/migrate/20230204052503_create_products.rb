af59ef7842621b4f501e43318ec5ff3c43aee333
ngetich kimutai daniel <daniel.ngetich@student.moringaschool.com>
2023-02-08 20:27:54 +0300

    add schema

--- db/migrate/20230204052503_create_products.rb
+++ db/migrate/20230204052503_create_products.rb
@@ -2,10 +2,9 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :imageurl
+      t.string :image
       t.string :description
       t.integer :price
-      t.string :discount
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_052503) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_093027) do
+  create_table "categories", force: :cascade do |t|
+    t.string "printing_branding"
+    t.string "general_clothing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "imageurl"
+    t.string "image"
     t.string "description"
     t.integer "price"
-    t.string "discount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
