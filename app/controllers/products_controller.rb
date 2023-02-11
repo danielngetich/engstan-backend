66a8ce558c02cfd1d83e49b7c5dbc6ff06a9ec65
ngetich kimutai daniel <daniel.ngetich@student.moringaschool.com>
2023-02-11 22:18:44 +0300

    add active storage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,12 @@ class ProductsController < ApplicationController
       render json: @product.errors, status: :unprocessable_entity
     end
   end
+  def index
+    @products = Product.all
+    render json: @products.as_json(include: { image_attachment: { only: :url } })
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
 
   private
 
