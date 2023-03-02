3750a447daa6595bef7f9abfac13c17961b579b0
ngetich kimutai daniel <daniel.ngetich@student.moringaschool.com>
2023-03-02 21:37:21 +0300

    add controller

--- app/models/branded_product.rb
+++
@@ -1,2 +0,0 @@
-class BrandedProduct < Product
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,4 @@ Rails.application.routes.draw do
       resources :products, only: [:index, :show, :create, :update, :destroy]
     end
   end
-  resources :products do
-    collection do
-      get '/products/:type', to: 'products#index'
-      get 'branded_products', to: 'products#branded_products'
-    end
-  end
 end
