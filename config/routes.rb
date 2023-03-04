1c542cc297dacb9fcf3edc7a89a5e86d026dff01
ngetich kimutai daniel <daniel.ngetich@student.moringaschool.com>
2023-03-04 07:36:47 +0300

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'login', to: 'users#new'
+  post 'login', to: 'users#create'
+  delete 'logout', to: 'users#logout'
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index, :show, :create, :update, :destroy]
